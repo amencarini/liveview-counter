a2eeb6dbb1da5518f2c0d749f11e1a5bf2d23a0f
Alessandro Mencarini <mencarini@fastmail.com>
2024-09-28 15:45:13 +0100

    Add counter functionality

--- lib/calculator_web/live/counter.ex
+++ lib/calculator_web/live/counter.ex
@@ -3,6 +3,20 @@ defmodule CalculatorWeb.CounterLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(:count, 0)}
+  end
+
+  @impl true
+  def handle_event("increment", _, socket) do
+    count = socket.assigns[:count]
+
+    {:noreply, socket |> assign(:count, count + 1)}
+  end
+
+  @impl true
+  def handle_event("decrement", _, socket) do
+    count = socket.assigns[:count]
+
+    {:noreply, socket |> assign(:count, count - 1)}
   end
 end
